5416185e7f67fd425d21380d1b40e6d6be126fb7
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2024-10-09 20:41:36 -0400

    el marbols

---
+++ SPOJ/Marbles.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#define endl "\n"
+#define sp " "
+#define all(x) x.begin(), x.end()
+#define MOD 1000000007
+#define f1(i, x) for(auto &i : x)
+#define f2(i, x, j) for(ll i = j; i < x; i++)
+#define raya() cout << endl << "====================================" << endl
+#define dbg(x) cerr << #x << ": " << x << endl;
+#define INF LLONG_MAX
+using namespace std;
+typedef long long ll;
+void solve(){
+    ll n, m;
+    cin >> n >> m;
+    ll ans = 1, x = n - m;
+    f2(i, n, x + 1){
+        ans *= i;
+        ans /= (i - x);
+    }
+    cout << ans << endl;
+    // maldito codigo asqueroso porque funcionas que te pasa no deberias el anterior era mejor que mrd te pasa
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie();
+    cout.tie();
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}  
