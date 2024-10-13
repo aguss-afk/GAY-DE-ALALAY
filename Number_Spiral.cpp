e9314a9bbae1b9d0eb4ab22b9a360ee32134201c
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2024-10-13 12:21:39 -0400

    pipipi no se pudo

---
+++ Number_Spiral.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define _USE_MATH_DEFINES
+#define endl "\n"
+#define sp " "
+#define MOD 1000000007
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define f1(i, x) for(auto &i : x)
+#define f2(i, x, j) for(ll i = j; i < x; i++)
+#define raya() cout << endl << "====================================" << endl
+#define dbg(x) cerr << #x << ": " << x << endl;
+#define INF LLONG_MAX
+using namespace std;
+typedef long long ll;
+void solve(){
+    int n, m;
+    cin >> n >> m; 
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
